006463b3f015e37d9762be90f56925f995a875b8
User <User@LAB2-15>
2021-10-11 14:35:18 +0300

    finish of openning the server

--- YadBeYadServer/Controllers/YadBeYadController.cs
+++ YadBeYadServer/Controllers/YadBeYadController.cs
@@ -4,12 +4,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YadBeYadServerBL.Models;
 
 namespace YadBeYadServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("YadBeYadAPI")]
     [ApiController]
     public class YadBeYadController : ControllerBase
     {
+        #region Add connection to the db context using dependency injection
+        AhiyaDBContext context;
+        public YadBeYadController(AhiyaDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+
     }
 }
--- YadBeYadServer/Startup.cs
+++ YadBeYadServer/Startup.cs
@@ -7,15 +7,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using YadBeYadServerBL.Models;
 namespace YadBeYadServer
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add Controllers and set the Json Serializer to handle loop referencing
+            services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions
+                        .ReferenceHandler = ReferenceHandler.Preserve);
+            //The following two commands set the Session state to work!
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(180);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            //The following set the connection string to the DB and DB context!
+            #region Add DB Context Support
+            string connectionString = this.Configuration.GetConnectionString("ContactsDB");
+
+            services.AddDbContext<AhiyaDBContext>(options => options
+                                                                .UseSqlServer(connectionString));
+            //.UseLazyLoadingProxies());
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +57,13 @@ namespace YadBeYadServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(); //Added to have the wwwroot folder and server to accept calls to static files
             app.UseRouting();
+            app.UseSession(); //Added to tell the server to use sessions!
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
